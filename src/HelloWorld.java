cb596a28618421c747607aede5d53add75157d69
David B. Collins <david.collins.hou@gmail.com>
2022-12-14 13:40:50 -0600

    revisiting basic java curriculum

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        int myFavoriteNumber = 13;
+        String myString = "DBCSEZ!";
+        double myNumber = 3.14;
+        int x = 4;
+        x += 5;
+        int y = Integer.MAX_VALUE;
+        y -= x;
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
